f526e78bfd24882a9360edebcb7fdf6bd873c674
Nathan Roset <nathroset97@gmail.com>
2026-01-29 10:40:07 +0100

    Resolves 4

--- lib/grid.dart
+++ lib/grid.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
-class InnerGrid extends Container {
+class InnerGrid extends StatefulWidget {
   int? s = 0;
   double? bx = 0;
 
   InnerGrid({super.key, required this.s, required this.bx});
 
   @override
+  State<StatefulWidget> createState() => _GridState();
+}
+
+class _GridState extends State<InnerGrid> {
+  bool isSelected = false;
+
+  void invertChoice() {
+    setState(() {
+      isSelected = !isSelected;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-        width: bx,
-        height: bx,
-        decoration:
-            BoxDecoration(border: Border.all(color: Colors.black, width: 0.3)),
-        child: Center(child: Text(s == 0 ? "" : s.toString())));
+        width: widget.bx,
+        height: widget.bx,
+        decoration: BoxDecoration(
+          color: isSelected
+              ? Colors.blueAccent.shade100.withAlpha(100)
+              : Colors.transparent,
+          border: Border.all(color: Colors.black, width: 0.3),
+        ),
+        child: InkWell(
+          onTap: () => invertChoice(),
+          child: Center(child: Text(widget.s == 0 || widget.s == null ? "" : widget.s.toString())),
+        ));
   }
 }
